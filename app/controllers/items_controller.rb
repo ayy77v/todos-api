f65089f75cee6bb3b828dcffe0f0ef71586fb5a3
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-07-23 14:46:58 +0800

    item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,42 @@
+class ItemsController < ApplicationController
+  before_action :set_todo
+  before_action :set_todo_item, only: [:show, :update, :destroy]
+
+	def index
+		json_response(@todos.items)
+    end
+
+    def show
+    	json_response(@item)
+    end
+
+    def create
+    	@todo.items.create(item_params)
+    	json_response(@todo, :created)
+    end
+
+    def update
+    	@item.destroy
+    	head :no_content
+    end
+
+    def destroy
+        @item.destroy
+        head :no_content
+    end
+
+    private
+    def item_params
+        params.permit(:name, :done)
+    end
+
+    def set_todo
+        @todo = Todo.find(params[:todo_id])
+    end
+
+    def set_todo_item
+        @item=@todo.items
+    end
+    
+
+end
---
+++ db/migrate/20190716024046_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :created_by
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
